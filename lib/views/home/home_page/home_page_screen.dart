3160a73780421c5e62f1e6a820bc6a12a417c957
kennethferdinandd <kennethferdinand03@gmail.com>
2022-12-11 22:25:59 +0700

    sisa merging

--- lib/views/home/home_page/home_page_screen.dart
+++ lib/views/home/home_page/home_page_screen.dart
@@ -8,14 +8,25 @@ import 'package:pusat_pengaduan/common/constant.dart';
 import 'package:pusat_pengaduan/utils/route.dart';
 import 'package:pusat_pengaduan/views/home/home_page/controller/home_page_controller.dart';
 import 'package:pusat_pengaduan/views/home/review/review_screen.dart';
+import 'package:pusat_pengaduan/views/home/review/review_model.dart';
 
 class HomePageScreen extends StatelessWidget {
   const HomePageScreen({super.key});
 
   getReview(request) async {
-    final response =
-        await request.get("https://pusat-pengaduan.up.railway.app/");
-    return response;
+    final response = await request.get("http://localhost:8000/json_review/");
+    // print(response);
+    List<Review> listReview = [];
+    for (var i in response) {
+      if (i != null) {
+        listReview.add(Review.fromJson(i));
+      }
+    }
+    // print(response[0]);
+    // Review re = Review.fromJson(response[0]);
+    // print(re);
+    // print(listReview);
+    return listReview;
   }
 
   @override
@@ -41,7 +52,6 @@ class HomePageScreen extends StatelessWidget {
             width: 300,
             height: 300,
           ),
-
           Container(
             margin: const EdgeInsets.fromLTRB(20, 20, 400, 10),
             child: ElevatedButton(
@@ -56,7 +66,6 @@ class HomePageScreen extends StatelessWidget {
                     style: TextStyle(
                         color: kPrimaryColor, fontWeight: FontWeight.w500))),
           ),
-
           Container(
               margin: const EdgeInsets.fromLTRB(20, 10, 400, 10),
               child: const Text(
@@ -66,54 +75,82 @@ class HomePageScreen extends StatelessWidget {
           GestureDetector(
             onTap: () => controller.navigateToBeritaDetailTiga(),
             child: Card(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10)),
                 elevation: 4,
                 child: SizedBox(
                   width: 200,
                   height: 100,
-                  child: Column(
+                  child: Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: const [
-                        Text(
-                            'Jaksa menjelaskan alasan Benny Tjokro dituntut pidana mati, salah satunya karena sebelumnya telah terlibat dalam kasus Jiwasraya hingga merugikan ASN Kementerian Pertahanan.'),
+                      children: [
+                        Image.asset(
+                          "assets/images/korupsi3.jpg",
+                          height: 80,
+                          width: 150,
+                        ),
+                        Flexible(
+                          child: Text(
+                            'Jaksa menjelaskan alasan Benny Tjokro dituntut pidana mati, salah satunya karena sebelumnya telah terlibat dalam kasus Jiwasraya hingga merugikan ASN Kementerian Pertahanan.',
+                          ),
+                        )
                       ]),
                 )),
           ),
           GestureDetector(
             onTap: () => controller.navigateToBeritaDetail(),
             child: Card(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10)),
                 elevation: 4,
                 child: SizedBox(
                   width: 200,
                   height: 100,
-                  child: Column(
+                  child: Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: const [
-                        Text(
-                            'Direktur Sosialisasi dan Kampanye Antikorupsi Komisi Pemberantasan Korupsi ( KPK ), Amir Arief menyoroti tingginya angka korupsi di sektor politik.'),
+                      children: [
+                        Image.asset(
+                          "assets/images/korupsi1.jpg",
+                          height: 80,
+                          width: 150,
+                        ),
+                        Flexible(
+                          child: Text(
+                              'Direktur Sosialisasi dan Kampanye Antikorupsi Komisi Pemberantasan Korupsi ( KPK ), Amir Arief menyoroti tingginya angka korupsi di sektor politik.'),
+                        )
                       ]),
                 )),
           ),
           GestureDetector(
             onTap: () => controller.navigateToBeritaDetailDua(),
             child: Card(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10)),
                 elevation: 4,
                 child: SizedBox(
                   width: 200,
                   height: 100,
-                  child: Column(
+                  child: Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: const [
-                        Text(
-                            'Harun Masiku seperti lenyap ditelan bumi. Politisi PDIP ini tak menampakkan diri di depan publik sejak komisioner KPU, Wahyu Setiawan ditangkap penyidik KPK Rabu 8 Januari lalu.'),
+                      children: [
+                        Image.asset(
+                          "assets/images/card1.jpg",
+                          height: 80,
+                          width: 150,
+                        ),
+                        Flexible(
+                          child: Text(
+                              'Harun Masiku seperti lenyap ditelan bumi. Politisi PDIP ini tak menampakkan diri di depan publik sejak komisioner KPU, Wahyu Setiawan ditangkap penyidik KPK Rabu 8 Januari lalu.'),
+                        )
                       ]),
                 )),
           ),
@@ -144,66 +181,67 @@ class HomePageScreen extends StatelessWidget {
                         color: kPrimaryColor, fontWeight: FontWeight.w500))),
           ),
           Container(
-              margin: const EdgeInsets.fromLTRB(20, 20, 400, 100),
+              margin: const EdgeInsets.fromLTRB(20, 20, 400, 20),
               child: const Text(
                 "Review",
                 style: TextStyle(fontSize: 20, color: kWhiteColor),
               )),
-          // FutureBuilder(future: getReview(request),
-          // builder: (context, AsyncSnapshot snapshot) {
-          //       if (snapshot.data == null) {
-          //         return const Center(child: CircularProgressIndicator());
-          //       } else {
-          //         if (!snapshot.hasData) {
-          //           return Column(
-          //             children: const [
-          //               Text("No Review Yet!", style: TextStyle(
-          //                 color: kWhiteColor,
-          //                 fontSize: 15
-          //               ),),
-          //               SizedBox(height: 8,)
-          //             ],
-          //           );
-          //         } else {
-          //           return ListView.builder(itemBuilder:
-          //           )
-          //         }
-          //       }
-          // },),
-          // Container(
-          //   child: ListView.builder(
-          //     itemCount: controller.data!.length,
-          //     itemBuilder: (_, index) => Container(
-          //       padding: EdgeInsets.fromLTRB(20, 0, 400, 100),
-          //       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-          //       decoration: BoxDecoration(
-          //         color: kWhiteColor,
-          //         borderRadius: BorderRadius.circular(15.0),
-          //         boxShadow: const [
-          //           BoxShadow(
-          //             color: kPrimaryColor,
-          //             blurRadius: 2.0
-          //           )
-          //         ]
-          //       ),
-          //       child: Column(
-          //         mainAxisAlignment: MainAxisAlignment.start,
-          //         crossAxisAlignment: CrossAxisAlignment.start,
-          //         children: const [
-          //           Text(
-          //             "Anon", style: const TextStyle(
-          //             fontSize: 15.0,
-          //             fontWeight: FontWeight.bold
-          //           ),
-          //           ),
-          //           const SizedBox(height: 10,),
-          //           Text("controller.data"),
-          //         ],
-          //       ),
-          //     ),
-          //
-          //       ),
-          // )
+          FutureBuilder(
+            future: getReview(request),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                if (!snapshot.hasData) {
+                  return Column(
+                    children: const [
+                      Text(
+                        "No Review Yet!",
+                        style: TextStyle(color: kWhiteColor, fontSize: 15),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      )
+                    ],
+                  );
+                } else {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (_, index) => Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 12),
+                      padding: const EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          color: kWhiteColor,
+                          borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: const [
+                            BoxShadow(color: kPrimaryColor, blurRadius: 2.0)
+                          ]),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${snapshot.data![index].fields.user}",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text("${snapshot.data![index].fields.comment}",
+                              style: const TextStyle(fontSize: 15))
+                        ],
+                      ),
+                    ),
+                  );
+
+                  // print(snapshot.data[0].fields.user);
+                  // return Text("ini data");
+                }
+              }
+            },
+          ),
         ],
       ),
     );
